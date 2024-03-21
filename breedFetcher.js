113bc3fa64cc283bac97b7c10e2d353931044a71
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-20 20:23:05 -0600

    commit on master

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,7 +4,7 @@ const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
 request(`${url}`, (error, response, body) => {
   if(error) {
-    onsole.error("Error", error)
+    console.error("Error", error)
   }
 
   const data = JSON.parse(body);
