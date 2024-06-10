518f0954d0d84a66b89c3cefcc75b16796f0d7d3
JordyNtambwe <jordyntambwe@gmail.com>
2024-06-09 21:46:49 -0600

    Refactor the code in breedFetcher.js by moving the main request logic into a function named fetchBreedDescription!

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,24 @@
 const needle = require("needle");
+
+const request = require("request");
+
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, _response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback("No cats found", null);
+      return;
+    }
+
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
